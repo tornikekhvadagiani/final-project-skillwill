b5026c41557799d3930d17d8c04cdf1750687f51
tornikekhvadagiani <tornikexvadagiani1@gmail.com>
2025-05-26 16:15:02 +0400

    add vercel

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const SEARCH_HISTORY_KEY = 'search_history';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      gcTime: 1000 * 60 * 30, // 30 minutes
+      staleTime: 1000 * 60 * 5, 
+      gcTime: 1000 * 60 * 30, 
     },
   },
 });
